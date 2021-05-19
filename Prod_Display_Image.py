6a858d2ef87db7e4687e2081662723eab4ad9de0
kevinatcraig <45322547+kevinatcraig@users.noreply.github.com>
2021-05-18 23:59:52 -0400

    commit prod display

--- Prod_Display_Image.py
+++ Prod_Display_Image.py
@@ -1,6 +1,6 @@
 import time
 import sys
-from rgbmatrix import RGBMatrix, RGBMatrixOptions
+from rgbmatrix import RGBMatrix, RGBMatrixOptions # https://github.com/hzeller/rpi-rgb-led-matrix
 from PIL import Image
 
 # Configuration for the matrix
@@ -9,16 +9,17 @@ options.rows = 32
 options.cols = 64
 options.chain_length = 1
 options.parallel = 1
-options.hardware_mapping = 'adafruit-hat' 
+options.hardware_mapping = 'adafruit-hat' # my hat hardware
 matrix = RGBMatrix(options=options)
 
+# update image passed from Prod_Prediction_Ticker.py
 while True:
     
     # image output by Prod_Prediction_Ticker
     image_file = 'Prod_Board_Photo.png'
     image = Image.open(image_file)
 
-    # Create a thumbnail that first our screen
+    # create a thumbnail that fits screen
     image.thumbnail((matrix.width, matrix.height), Image.ANTIALIAS)
     matrix.SetImage(image.convert('RGB'))
 
